c3856a310c49edaea61d30a594b415ec21f63815
lamis ghoualmi <56949028+lamisghoualmi@users.noreply.github.com>
2022-06-18 17:38:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import numpy as np
 import pandas  as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-import matplotlib.pyplot as plt
 from sklearn import preprocessing
 import pickle
 from sklearn.linear_model import LogisticRegression
